1f505cf932190f676938b65cf984f3dcee34af33
jennymyllymaa <jennyinen@gmail.com>
2025-08-05 18:02:31 +0300

    Add: New component HolderCardContainer.

--- token-customizer-tool/src/pages/Home/Home.jsx
+++ token-customizer-tool/src/pages/Home/Home.jsx
@@ -1,8 +1,25 @@
+import HolderCardContainer from "../../components/HolderCardContainer/HolderCardContainer";
+import Card from "@mui/material/Card";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+
 export default function Home() {
 
     return (
-        <div>
-            <p>Home</p>
-        </div>
+        <Container>
+            <Card>
+                <p>Choose how many tokens you want</p>
+            </Card>
+            <Box>
+                <HolderCardContainer/>
+            </Box>
+            <Card>
+                <p>Choose the base color of your deck</p>
+                <p>You get to freely customize this later!</p>
+            </Card>
+            <Box>
+                <p>Manavärit tänne</p>
+            </Box>
+        </Container>
     );
 };
