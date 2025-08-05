72a5e78064e217fa4894b8d1e52ccfd0f2e9e8c1
jennymyllymaa <jennyinen@gmail.com>
2025-08-05 20:09:36 +0300

    Add: Container for HolderCard components.

---
+++ token-customizer-tool/src/components/HolderCardContainer/styles.js
@@ -0,0 +1,18 @@
+const cardStyle = {
+    //Layout
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: "center",
+    padding: "1em",
+};
+
+const contentStyle = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignContent: "center",
+    maxWidth: "100%",
+};
+
+export { cardStyle, contentStyle };
