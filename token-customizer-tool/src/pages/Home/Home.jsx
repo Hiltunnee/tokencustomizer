686a1bb4398e1680f22140e872d4b8e9d1ca5fcb
jennymyllymaa <jennyinen@gmail.com>
2025-08-05 20:11:52 +0300

    Refactor.

--- token-customizer-tool/src/pages/Home/Home.jsx
+++ token-customizer-tool/src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import HolderCardContainer from "../../components/HolderCardContainer/HolderCardContainer";
+import { useState } from "react";
 import Card from "@mui/material/Card";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
