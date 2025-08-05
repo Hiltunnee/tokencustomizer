efc6595da1e14f792f2d46a1b35ace4354764960
jennymyllymaa <jennyinen@gmail.com>
2025-08-05 19:29:14 +0300

    Add: Context wrappers.

--- token-customizer-tool/src/App.jsx
+++ token-customizer-tool/src/App.jsx
@@ -1,14 +1,25 @@
-//Components
 import TabsRouter from "./components/TabsRouter/TabsRouter";
-
-//Style
 import './App.css'
+import { useState } from "react";
 
+//Contexts
+import { ThemeContext } from "./contexts/ThemeContext";
+import { TokensContext } from "./contexts/TokensContext";
+import { HolderContext } from "./contexts/HolderContext";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const [tokenSet, setTokenSet] = useState([]);
+  const [selectedHolder, setSelectedHolder] = useState();
 
   return (
-      <TabsRouter />
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      <TokensContext.Provider value={{tokenSet, setTokenSet}}>
+        <HolderContext.Provider value={{selectedHolder, setSelectedHolder}}>
+          <TabsRouter />
+        </HolderContext.Provider>
+      </TokensContext.Provider>
+    </ThemeContext.Provider>
   )
 }
 
