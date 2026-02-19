93b48e432f5ebaf513f6be2dc9cae7872cb7b904
jennymyllymaa <jennyinen@gmail.com>
2026-02-19 14:36:25 +0200

    Fix: Token border.

--- token-customizer-tool/src/components/Token/Token.jsx
+++ token-customizer-tool/src/components/Token/Token.jsx
@@ -8,7 +8,7 @@ export default function Token({text, color, borderColor, isNumberToken, onClick}
   return (
     <Box component="svg" viewBox=" 8 20 84 61" sx={svgStyle} onClick={onClick}>
       <polygon points="50 20 92 37 92 81 50 68 8 81 8 37" fill={borderColor}/>
-      <polygon points="50 25 88 40 88 76 50 63 12 76 12 40" fill={color}/>
+      <polygon points="50 25 88 40 88 76 50 64 12 76 12 40" fill={color}/>
       <text x="50" y="53" textAnchor="middle" dominantBaseline="middle" fill={borderColor} style={isNumberToken ? numberTokenStyle : textTokenStyle}>
         {text}
       </text>
