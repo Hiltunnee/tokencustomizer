eb6f6794d1b99abdccb89e39494ed26ed36e2dce
jennymyllymaa <jennyinen@gmail.com>
2026-02-27 18:23:22 +0200

    Add: Base for new component.

--- token-customizer-tool/src/components/TokenCustomContainer/TokenCustomContainer.jsx
+++ token-customizer-tool/src/components/TokenCustomContainer/TokenCustomContainer.jsx
@@ -11,7 +11,7 @@ export default function TokenCustomContainer({tokens, onTokenClick}) {
         <Card>
             <Box sx={{display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(120px, 1fr))", gap: 5, justifyItems: "start", padding: "2em"}}>
                 {tokens.map(token => (
-                    <TokenCustom key={token.text} text={token.text} baseColor={token.baseColorCode} borderColor={token.borderColorCode} isNumberToken={token.isNumberToken} amount={token.amount} onClick={() => onTokenClick(token)}  />
+                    <TokenCustom key={`${token.text}-${token.baseColor}-${token.borderColor}`} text={token.text} baseColor={token.baseColorCode} borderColor={token.borderColorCode} isNumberToken={token.isNumberToken} amount={token.amount} onClick={() => onTokenClick(token)}  />
                 ))}
             </Box>
         </Card>
