f12ebe4adf24f666cec98b05f22a678625bd0cf9
jennymyllymaa <jennyinen@gmail.com>
2026-03-08 12:05:21 +0200

    Refactor: Whole holder card to clickable.

--- token-customizer-tool/src/components/HolderCard/HolderCard.jsx
+++ token-customizer-tool/src/components/HolderCard/HolderCard.jsx
@@ -2,17 +2,22 @@ import Card from "@mui/material/Card";
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Radio from "@mui/material/Radio";
+import Box from "@mui/material/Box";
 
 //Style
-import { holderCardStyle, radioButtonStyle } from "./styles";
+import { holderCardStyle, radioButtonStyle, boxStyle, labelStyle, imageStyle } from "./styles";
 
 export default function HolderCard({id, name, imagePath, alt}) {
 
     return (
         <Card sx={holderCardStyle}>
-            <p>{name}</p>
-            <img src={imagePath} alt={alt}></img>
-            <FormControlLabel value={id} sx={radioButtonStyle} control={<Radio/>}/>
+            <FormControlLabel value={id} sx={labelStyle} control={<Radio sx={radioButtonStyle}/>} label={
+                <Box sx={boxStyle}>
+                    <p>{name}</p>
+                    <img src={imagePath} style={imageStyle} alt={alt}></img>
+                </Box>
+            }
+            />
         </Card>
     );
 };
