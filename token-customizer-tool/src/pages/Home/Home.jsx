d52855e5a4ecb7a177bd376876019cb11a2e7bf0
jennymyllymaa <jennyinen@gmail.com>
2026-06-25 21:41:38 +0300

    Add welcome dialog to beginning.

--- token-customizer-tool/src/pages/Home/Home.jsx
+++ token-customizer-tool/src/pages/Home/Home.jsx
@@ -3,6 +3,10 @@ import ManaColorContainer from "../../components/ManaColorContainer/ManaColorCon
 import Card from "@mui/material/Card";
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogActions from "@mui/material/DialogActions";
+import logo_white from "../../../public/logo_white.png";
 import Box from "@mui/material/Box";
 import { pageStyle, textCardStyle } from "./styles";
 import { useState, useContext, useEffect } from "react";
@@ -14,36 +18,37 @@ import { useNavigate } from "react-router";
 
 export default function Home() {
     //Basic set
-    const [selectedManaColor, setSelectedManaColor] = useState(7); //Tämä
+    //const [selectedManaColor, setSelectedManaColor] = useState(7); //Tämä
     const { selectedHolder, setSelectedHolder } = useContext(HolderContext);
     const { tokenSet, setTokenSet } = useContext(TokensContext);
+    const [openWelcomeDialog, setOpenWelcomeDialog] = useState(true);
     const navigate = useNavigate();
 
     //Holder id: 1.16x, 2.32x, 3.48x 
     //Mana id: 1.Black, 2.White, 3.Blue, 4.Green, 5.Red, 6.Colorless, 7.Basic
-    const searchMatchingPreset = () => {
-        const holderSizeFromId = (selectedHolder == 3) ? 48 : (selectedHolder == 2) ? 32 : 16
-        const matchingColorSets = setInventory.presets.filter(preset => preset.ManaId == selectedManaColor).flatMap(preset => preset.sets);
-        const matchingSet = matchingColorSets.filter(preset => preset.holderSize == holderSizeFromId).flatMap(preset => preset);
-        if (matchingSet.length == 0) {
-            console.log("PRESET PUUTTUU!");
-        } else {
-            matchingSet[0].tokens.forEach(token => {
-                token.baseColor = "Black",
-                token.baseColorCode = "#000000",
-                token.borderColor = "White",
-                token.borderColorCode = "#ffffff",
-                token.text = token.text.toUpperCase()
-            });
-        };
+    // const searchMatchingPreset = () => {
+    //     const holderSizeFromId = (selectedHolder == 3) ? 48 : (selectedHolder == 2) ? 32 : 16
+    //     const matchingColorSets = setInventory.presets.filter(preset => preset.ManaId == selectedManaColor).flatMap(preset => preset.sets);
+    //     const matchingSet = matchingColorSets.filter(preset => preset.holderSize == holderSizeFromId).flatMap(preset => preset);
+    //     if (matchingSet.length == 0) {
+    //         console.log("PRESET PUUTTUU!");
+    //     } else {
+    //         matchingSet[0].tokens.forEach(token => {
+    //             token.baseColor = "Black",
+    //             token.baseColorCode = "#000000",
+    //             token.borderColor = "White",
+    //             token.borderColorCode = "#ffffff",
+    //             token.text = token.text.toUpperCase()
+    //         });
+    //     };
 
-        //Onko kyseessä eka holder
-        if (tokenSet.length == 0) {
-            setTokenSet(matchingSet);
-        } else {
-            setTokenSet([...matchingSet, ...tokenSet]);
-        }
-    };
+    //     //Onko kyseessä eka holder
+    //     if (tokenSet.length == 0) {
+    //         setTokenSet(matchingSet);
+    //     } else {
+    //         setTokenSet([...matchingSet, ...tokenSet]);
+    //     }
+    // };
 
     const moveToCustomization = () => {
         // searchMatchingPreset();
@@ -81,6 +86,18 @@ export default function Home() {
                     </Button>
                 </Stack>
             </Stack>
+
+            <Dialog
+                open={openWelcomeDialog}
+                onClose={() => {setOpenWelcomeDialog(false)}}
+                aria-labelledby="welcome-dialog-title"
+                aria-describedby="welcome-dialog"
+                PaperProps={{ sx: { backgroundColor: "var(--background-secondary)", margin: "30px" }}}
+            >
+                <img src={logo_white} alt="Logo" style={{width: "300px", height: "auto", margin: "20px 30px 0px 30px", display: "block"}} />
+                <p>Welcome to Token Customizer!</p>
+                <Button variant="contained" sx={{ width: "50%", marginBottom: "30px", alignSelf: "center" }} onClick={() => {setOpenWelcomeDialog(false)}}>Start</Button>
+            </Dialog>
         </Container>
     );
 };
