1be248b5ccbb83882ce92780808ba6855a8cc5e1
jennymyllymaa <jennyinen@gmail.com>
2026-06-26 12:25:36 +0300

    Switch Icon to Text for Safari.

--- token-customizer-tool/src/components/AddToken/AddToken.jsx
+++ token-customizer-tool/src/components/AddToken/AddToken.jsx
@@ -1,4 +1,3 @@
-import AddIcon from '@mui/icons-material/Add';
 import Typography from '@mui/material/Typography';
 import Box from "@mui/material/Box";
 
@@ -10,7 +9,9 @@ function AddTokenBase({onClick}) {
     <Box component="svg" viewBox=" 8 20 84 61" sx={svgStyle} onClick={onClick}>
       <polygon points="50 20 92 37 92 81 50 68 8 81 8 37" fill="#7a7a7adc"/>
       <polygon points="50 25 88 40 88 76 50 64 12 76 12 40" fill="#e4d8d8c4"/>
-      <AddIcon color="black" x="42" y="48" transform="scale(0.6)" />
+      <text x="50" y="48" textAnchor="middle" dominantBaseline="middle" fontSize="50" fill="black">
+        +
+      </text>
     </Box>
   );
 }
